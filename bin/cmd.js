c652cc9fe5f14c847e24fb01e2fa8ed71e2f9403
James Halliday <mail@substack.net>
2014-01-05 15:07:29 -0800

    2-space for consistency

--- bin/cmd.js
+++ bin/cmd.js
@@ -16,101 +16,101 @@ if (argv.h || argv.help) return usage(0);
 
 var files = argv._;
 if (files.length === 0) {
-    files = fs.readdirSync(path.join(__dirname, '..', 'bench'))
-        .filter(function (x) { return path.extname(x) === '.js' })
-        .map(function (x) { return path.join(__dirname, '..', 'bench', x) })
-    ;
+  files = fs.readdirSync(path.join(__dirname, '..', 'bench'))
+    .filter(function (x) { return path.extname(x) === '.js' })
+    .map(function (x) { return path.join(__dirname, '..', 'bench', x) })
+  ;
 }
 
 var http = argv.r || 'http';
 if (!http) return usage(1);
 try { require.resolve(http) }
 catch (e) {
-    http = path.resolve(http);
-    require.resolve(http);
+  http = path.resolve(http);
+  require.resolve(http);
 }
 
 if (argv.b && files.length > 1) {
-    console.error('-b only works with a single benchmark file');
-    return process.exit(1);
+  console.error('-b only works with a single benchmark file');
+  return process.exit(1);
 }
 
 var benchmarks = {};
 
 (function next () {
-    if (files.length === 0) return printSummary();
-    var file = files.shift();
-    var name = path.basename(file).split('.')[0];
-    var b = benchmarks[name] = {};
-    
-    var ps = spawn(process.execPath, [ file, http ]);
-    ps.stderr.pipe(process.stderr);
-    
-    var links = [], first = true;
-    if (argv.b) {
-        ps.stdout.pipe(split()).pipe(through(function (line) {
-            if (!first) return;
-            first = false;
-            var u = url.parse(line.trim());
-            var base = u.protocol + '//' + u.host;
-            
-            links = [].concat(argv.b).map(function (x) {
-                return url.resolve(base, x);
-            });
-            nextLink();
-        }));
+  if (files.length === 0) return printSummary();
+  var file = files.shift();
+  var name = path.basename(file).split('.')[0];
+  var b = benchmarks[name] = {};
+  
+  var ps = spawn(process.execPath, [ file, http ]);
+  ps.stderr.pipe(process.stderr);
+  
+  var links = [], first = true;
+  if (argv.b) {
+    ps.stdout.pipe(split()).pipe(through(function (line) {
+      if (!first) return;
+      first = false;
+      var u = url.parse(line.trim());
+      var base = u.protocol + '//' + u.host;
+      
+      links = [].concat(argv.b).map(function (x) {
+        return url.resolve(base, x);
+      });
+      nextLink();
+    }));
+  }
+  else {
+    ps.stdout.pipe(split()).pipe(through(function (line) {
+      links.push(line.toString().trim());
+      if (first) nextLink();
+      first = false;
+    }));
+  }
+  
+  function nextLink () {
+    if (links.length === 0) {
+      ps.kill();
+      return next();
     }
-    else {
-        ps.stdout.pipe(split()).pipe(through(function (line) {
-            links.push(line.toString().trim());
-            if (first) nextLink();
-            first = false;
-        }));
-    }
     
-    function nextLink () {
-        if (links.length === 0) {
-            ps.kill();
-            return next();
-        }
-        
-        var uri = links.shift();
-        var u = url.parse(uri);
-        var b = benchmark(uri, argv.t || 10);
-        var sname = name + '  ' + u.path;
-        
-        process.stdout.write(sprintf('\r%-24s %3d %%', sname, 0));
-        b.on('percent', function (percent) {
-            process.stdout.write(sprintf(
-                '\r%-24s %3d %%', sname, percent
-            ));
-        });
-        
-        b.on('error', function (err) { console.error(err) });
-        b.on('stderr', function (err) { console.error(err) });
-        
-        b.on('results', function (results) {
-            var rps = parseFloat(results['requests per second']);
-            process.stdout.write(sprintf('\r%-24s %f\x1b[K\r\n', sname, rps));
-            
-            benchmarks[name] = {
-                'requests per second': rps,
-                'time per request': parseFloat(results['time per request'])
-            };
-        });
-        
-        b.on('exit', nextLink);
-    }
+    var uri = links.shift();
+    var u = url.parse(uri);
+    var b = benchmark(uri, argv.t || 10);
+    var sname = name + '  ' + u.path;
+    
+    process.stdout.write(sprintf('\r%-40s %3d %%', sname, 0));
+    b.on('percent', function (percent) {
+      process.stdout.write(sprintf(
+        '\r%-40s %3d %%', sname, percent
+      ));
+    });
+    
+    b.on('error', function (err) { console.error(err) });
+    b.on('stderr', function (err) { console.error(err) });
+    
+    b.on('results', function (results) {
+      var rps = parseFloat(results['requests per second']);
+      process.stdout.write(sprintf('\r%-40s %f\x1b[K\r\n', sname, rps));
+      
+      benchmarks[name] = {
+        'requests per second': rps,
+        'time per request': parseFloat(results['time per request'])
+      };
+    });
+    
+    b.on('exit', nextLink);
+  }
 })();
 
 function printSummary () {
-    // todo
+  // todo
 }
 
 function usage (code) {
-    var r = fs.createReadStream(__dirname + '/usage.txt');
-    r.pipe(process.stdout);
-    r.on('end', function () {
-        if (code !== 0) process.exit(code);
-    });
+  var r = fs.createReadStream(__dirname + '/usage.txt');
+  r.pipe(process.stdout);
+  r.on('end', function () {
+    if (code !== 0) process.exit(code);
+  });
 }
--- index.js
+++ index.js
@@ -7,57 +7,57 @@ var EventEmitter = require('events').EventEmitter;
 module.exports = benchmark;
 
 function benchmark (uri, t, cb) {
-    var u = url.parse(uri);
-    var host = u.hostname === 'localhost' ? '127.0.0.1' : u.host;
-    uri = u.protocol + '//' + host + (u.port ? ':' + u.port : '') + u.path;
-    var bench = new EventEmitter;
-    if (cb) {
-        bench.on('results', function (res) { cb(null, res) });
-        bench.on('error', cb);
+  var u = url.parse(uri);
+  var host = u.hostname === 'localhost' ? '127.0.0.1' : u.host;
+  uri = u.protocol + '//' + host + (u.port ? ':' + u.port : '') + u.path;
+  var bench = new EventEmitter;
+  if (cb) {
+    bench.on('results', function (res) { cb(null, res) });
+    bench.on('error', cb);
+  }
+  
+  var results = {};
+  
+  var ab = spawn('ab', [ '-c', 100, '-t', t, uri ]);
+  ab.stdout.pipe(split()).pipe(through(function (line) {
+    var m;
+    if (m = /^(\S[^:]*):\s{2,}(.+)/.exec(line)) {
+      var key = m[1].toLowerCase();
+      var value = m[2].trim();
+      results[key] = value;
     }
+  }));
+  
+  var t0 = Date.now();
+  var iv = setInterval(function () {
+    var percent = Math.floor(100 * (Date.now() - t0) / 1000 / t);
+    bench.emit('percent', Math.min(100, percent));
+  }, 100);
+  
+  ab.stderr.pipe(split()).pipe(through(function (line) {
+    var m;
+    if (m = /^Completed (\d+)/.exec(line)) {
+      bench.emit('completed', parseInt(m[1]));
+    }
+    else if (m = /^Finished (\d+)/.exec(line)) {
+      bench.emit('finished', parseInt(m[1]));
+    }
+    else if (/\S/.test(line)) {
+      bench.emit('stderr', line);
+    }
+  }));
+  
+  ab.on('exit', function (code) {
+    clearInterval(iv);
     
-    var results = {};
-    
-    var ab = spawn('ab', [ '-c', 100, '-t', t, uri ]);
-    ab.stdout.pipe(split()).pipe(through(function (line) {
-        var m;
-        if (m = /^(\S[^:]*):\s{2,}(.+)/.exec(line)) {
-            var key = m[1].toLowerCase();
-            var value = m[2].trim();
-            results[key] = value;
-        }
-    }));
-    
-    var t0 = Date.now();
-    var iv = setInterval(function () {
-        var percent = Math.floor(100 * (Date.now() - t0) / 1000 / t);
-        bench.emit('percent', Math.min(100, percent));
-    }, 100);
-    
-    ab.stderr.pipe(split()).pipe(through(function (line) {
-        var m;
-        if (m = /^Completed (\d+)/.exec(line)) {
-            bench.emit('completed', parseInt(m[1]));
-        }
-        else if (m = /^Finished (\d+)/.exec(line)) {
-            bench.emit('finished', parseInt(m[1]));
-        }
-        else if (/\S/.test(line)) {
-            bench.emit('stderr', line);
-        }
-    }));
-    
-    ab.on('exit', function (code) {
-        clearInterval(iv);
-        
-        if (code === 0) {
-            bench.emit('results', results);
-        }
-        else {
-            bench.emit('error', new Error('non-zero exit code from `ab`'));
-        }
-        bench.emit('exit', code);
-    });
-    
-    return bench;
+    if (code === 0) {
+      bench.emit('results', results);
+    }
+    else {
+      bench.emit('error', new Error('non-zero exit code from `ab`'));
+    }
+    bench.emit('exit', code);
+  });
+  
+  return bench;
 }

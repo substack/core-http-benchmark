4f4e8bdd37c546c61efa7f4f30763ac0d13141d4
James Halliday <mail@substack.net>
2014-01-05 12:16:04 -0800

    use the actual simple benchmark from node core

--- bench/simple.js
+++ bench/simple.js
@@ -1,34 +1,115 @@
 var http = require(process.argv[2]);
 
-var server = http.createServer(function (req, res) {
-    res.end('beep boop\n');
-});
-server.listen(0);
+var fixed = makeString(20 * 1024, 'C'),
+    storedBytes = {},
+    storedBuffer = {},
+    storedUnicode = {};
 
-server.on('listening', function () {
+var useDomains = process.env.NODE_USE_DOMAINS;
+
+// set up one global domain.
+if (useDomains) {
+  var domain = require('domain');
+  var gdom = domain.create();
+  gdom.on('error', function(er) {
+    console.error('Error on global domain', er);
+    throw er;
+  });
+  gdom.enter();
+}
+
+var server = http.createServer(function (req, res) {
+  if (useDomains) {
+    var dom = domain.create();
+    dom.add(req);
+    dom.add(res);
+  }
+
+  var commands = req.url.split('/');
+  var command = commands[1];
+  var body = '';
+  var arg = commands[2];
+  var n_chunks = parseInt(commands[3], 10);
+  var status = 200;
+
+  if (command == 'bytes') {
+    var n = ~~arg;
+    if (n <= 0)
+      throw new Error('bytes called with n <= 0')
+    if (storedBytes[n] === undefined) {
+      storedBytes[n] = makeString(n, 'C');
+    }
+    body = storedBytes[n];
+
+  } else if (command == 'buffer') {
+    var n = ~~arg;
+    if (n <= 0)
+      throw new Error('buffer called with n <= 0');
+    if (storedBuffer[n] === undefined) {
+      storedBuffer[n] = new Buffer(n);
+      for (var i = 0; i < n; i++) {
+        storedBuffer[n][i] = 'C'.charCodeAt(0);
+      }
+    }
+    body = storedBuffer[n];
+
+  } else if (command == 'unicode') {
+    var n = ~~arg;
+    if (n <= 0)
+      throw new Error('unicode called with n <= 0');
+    if (storedUnicode[n] === undefined) {
+      storedUnicode[n] = makeString(n, '\u263A');
+    }
+    body = storedUnicode[n];
+
+  } else if (command == 'quit') {
+    res.connection.server.close();
+    body = 'quitting';
+
+  } else if (command == 'fixed') {
+    body = fixed;
+
+  } else if (command == 'echo') {
+    res.writeHead(200, { 'Content-Type': 'text/plain',
+                         'Transfer-Encoding': 'chunked' });
+    req.pipe(res);
+    return;
+
+  } else {
+    status = 404;
+    body = 'not found\n';
+  }
+
+  // example: http://localhost:port/bytes/512/4
+  // sends a 512 byte body in 4 chunks of 128 bytes
+  if (n_chunks > 0) {
+    res.writeHead(status, { 'Content-Type': 'text/plain',
+                            'Transfer-Encoding': 'chunked' });
+    // send body in chunks
+    var len = body.length;
+    var step = Math.floor(len / n_chunks) || 1;
+
+    for (var i = 0, n = (n_chunks - 1); i < n; ++i) {
+      res.write(body.slice(i * step, i * step + step));
+    }
+    res.end(body.slice((n_chunks - 1) * step));
+  } else {
+    var content_length = body.length.toString();
+
+    res.writeHead(status, { 'Content-Type': 'text/plain',
+                            'Content-Length': content_length });
+    res.end(body);
+  }
+});
+
+function makeString(size, c) {
+  var s = '';
+  while (s.length < size) {
+    s += c;
+  }
+  return s;
+}
+
+server.listen(0, function () {
     console.log('http://localhost:' + server.address().port);
 });
-
-/*
-var bench = common.createBenchmark(main, {
-  // unicode confuses ab on os x.
-  type: ['bytes', 'buffer'],
-  length: [4, 1024, 102400],
-  chunks: [0, 1, 4],  // chunks=0 means 'no chunked encoding'.
-  c: [50, 500]
-});
-
-function main(conf) {
-  process.env.PORT = PORT;
-  var spawn = require('child_process').spawn;
-  var server = require('../http_simple.js');
-  setTimeout(function() {
-    var path = '/' + conf.type + '/' + conf.length + '/' + conf.chunks;
-    var args = ['-r', 5000, '-t', 8, '-c', conf.c];
-
-    bench.http(path, args, function() {
-      server.close();
-    });
-  }, 2000);
-}
-*/
